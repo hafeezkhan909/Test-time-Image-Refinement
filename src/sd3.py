a1fa83f7a4a2556adb2d64e35acadf3de21d70c8
hafeezkhan909 <f20190091@dubai.bits-pilani.ac.in>
2025-04-25 02:35:25 -0400

    sd3 is ready

--- src/sd3.py
+++ src/sd3.py
@@ -7,7 +7,7 @@ from diffusers import StableDiffusion3Pipeline
 from qwen_integration import get_refined_prompt
 from PIL import Image
 import shutil
-
+# Ready for SD3 run
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--prompts_file", type=str, default="filtered_prompts.json")
